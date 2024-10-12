454f0aebd35b878de2369adf5010340cfed15413
Ahmed Adel <a7med3del1973@gmail.com>
2024-10-13 00:46:43 +0300

    Added Filter Authorization Login

---
+++ Week02/Web & HTTP/servelt-task/src/main/java/org/example/Servlet/AuthFilter.java
@@ -0,0 +1,35 @@
+package org.example.Servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/catalog/*")
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+
+        if (session != null && session.getAttribute("user") != null) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
